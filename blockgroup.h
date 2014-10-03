4efc27b77402c5fd350ae055e8434f52f877ed1c
cabusar <cabusar@users.noreply.github.com>
2014-10-03 13:36:57 +0200

    Create blockgroup.h

---
+++ blockgroup.h
@@ -0,0 +1,11 @@
+struct Block_Group_Descriptor 
+{
+  int bg_block_bitmap;
+  int   bg_inode_bitmap;
+  int   bg_inode_table;
+  short bg_free_blocks_count;
+  short bg_free_inodes_count;
+  short bg_used_dirs_count;
+  short bg_pad;
+  char  bg_reserved[12];
+};
